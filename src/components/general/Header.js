10731760726f2a095039b4ca64ea3a62161c11cd
André Ottosen <andre.ottosen@no.experis.com>
2020-09-28 14:33:02 +0200

    Implemented navigation icon with Microsoft Fluent UI API.

--- src/components/general/Header.js
+++ src/components/general/Header.js
@@ -1,13 +1,22 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 import { Link, NavLink } from 'react-router-dom';
+
+import { Icon } from '@fluentui/react/lib/Icon';
+import { initializeIcons } from '@uifabric/icons';
+initializeIcons();
 
 function Header() {
     return (
         <nav className="nav">
-            <h1>Header</h1>
-            {/*<NavLink activeClassName="active-menu-item" className="link-menu" to="/active-booking"><Icon iconName="DateTime2" />Active Booking</NavLink> */}
+            <ul>
+                <li><NavLink activeClassName="active-menu-item" className="link-menu" to="/"><section>My Profile <Icon iconName="Rocket" /></section></NavLink> </li>
+            </ul>
+           
         </nav>
     )
 }
+
+
 
 export default Header;
